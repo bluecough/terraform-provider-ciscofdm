dbd6386bdceed1b10174c7f1a32d6983621b6e49
George Seeto <9311254+bluecough@users.noreply.github.com>
2020-01-22 22:08:59 -0500

    Changing Structure to match standards

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/bluecough/terraform-provider-ciscofdm/ciscofdm"
 )
 
 func main() {
