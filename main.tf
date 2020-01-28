f66dde61935aaa010b29b350b5978e40a215bcb8
George Seeto <9311254+bluecough@users.noreply.github.com>
2020-01-28 18:13:41 -0500

    Basic Access Rule functionality working

--- main.tf
+++ main.tf
@@ -9,39 +9,43 @@ provider "ciscofdm" {
 #  address = "1.2.3.4"
 #}
 
-resource "ciscofdm_networkobject" "myobject" {
-  name = "GS-Terraform"
-  subtype = "HOST"
-  value = "2.2.2.3"
-}
+#resource "ciscofdm_networkobject" "myobject" {
+#  name = "GS-Terraform"
+#  subtype = "HOST"
+#  value = "2.2.2.3"
+#}
 
-resource "ciscofdm_networkobjectgroup" "mygroup" {
-  name = "NetworkGroupGS"
-  objects {
-     netname = "any-ipv6"
-     type = "networkobject"
-  }
-  objects {
-     netname = "any-ipv4"
-     type = "networkobject"
-  }
-  type = "networkobjectgroup"
-}
-resource "ciscofdm_portobject" "GSPORT_4444" {
-  name = "GSPORT 4444-5000"
-  layer4 = "TCP"
-  port = "4444"
-}
-resource "ciscofdm_portobjectgroup" "myportobjectgroup" {
-  name = "GSPORTGROUP"
-  description = "My Port Group"
-  type = "portobjectgroup"
-  objects {
-    name = "FTP"
-    type = "tcpportobject"
-  }
-  objects {
-    name = "GTP_PORTS-2123"
-    type = "tcpportobject"
-  }
+#resource "ciscofdm_networkobjectgroup" "mygroup" {
+#  name = "NetworkGroupGS"
+#  objects {
+#     netname = "any-ipv6"
+#     type = "networkobject"
+#  }
+#  objects {
+#     netname = "any-ipv4"
+#     type = "networkobject"
+#  }
+#  type = "networkobjectgroup"
+#}
+#resource "ciscofdm_portobject" "GSPORT_4444" {
+#  name = "GSPORT 4444-5000"
+#  layer4 = "TCP"
+#  port = "4444"
+#}
+#resource "ciscofdm_portobjectgroup" "myportobjectgroup" {
+#  name = "GSPORTGROUP"
+#  description = "My Port Group"
+#  type = "portobjectgroup"
+#  objects {
+#    name = "FTP"
+#    type = "tcpportobject"
+#  }
+#  objects {
+#    name = "GTP_PORTS-2123"
+#    type = "tcpportobject"
+#  }
+#}
+resource "ciscofdm_accessrule" "myaccessrules" {
+  name = "GSTERRAFORMRULE001"
+  ruleaction = "PERMIT"  
 }
--- resource_accessrule.go
+++ resource_accessrule.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/bluecough/go-ftd"
+	"log"
 )
 
 func resourceAccessRule() *schema.Resource {
@@ -12,6 +14,10 @@ func resourceAccessRule() *schema.Resource {
 		Delete: resourceAccessRuleDelete,
 
 		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
 			"ruleid": &schema.Schema{
 				Type:     schema.TypeInt,
 				Optional: true,
@@ -31,7 +37,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -55,7 +61,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -79,7 +85,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -103,7 +109,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -127,7 +133,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -151,7 +157,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -162,7 +168,7 @@ func resourceAccessRule() *schema.Resource {
 			},
 			"ruleaction": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
+				Required: true,
 			},
 			"eventlogaction": &schema.Schema{
 				Type:     schema.TypeString,
@@ -170,7 +176,7 @@ func resourceAccessRule() *schema.Resource {
 			},
 			"vlantags": &schema.Schema{
 				Type:     schema.TypeSet,
-				Required: true,
+				Optional: true,
 				Elem: &schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"id": &schema.Schema{
@@ -183,7 +189,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -194,7 +200,7 @@ func resourceAccessRule() *schema.Resource {
 			},
 			"users": &schema.Schema{
 				Type:     schema.TypeSet,
-				Required: true,
+				Optional: true,
 				Elem: &schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"id": &schema.Schema{
@@ -207,7 +213,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -218,7 +224,7 @@ func resourceAccessRule() *schema.Resource {
 			},
 			"intrusionpolicy": &schema.Schema{
 				Type:     schema.TypeSet,
-				Required: true,
+				Optional: true,
 				Elem: &schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"id": &schema.Schema{
@@ -231,7 +237,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -242,7 +248,7 @@ func resourceAccessRule() *schema.Resource {
 			},
 			"filepolicy": &schema.Schema{
 				Type:     schema.TypeSet,
-				Required: true,
+				Optional: true,
 				Elem: &schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"id": &schema.Schema{
@@ -255,7 +261,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -270,7 +276,7 @@ func resourceAccessRule() *schema.Resource {
 			},
 			"syslogserver": &schema.Schema{
 				Type:     schema.TypeSet,
-				Required: true,
+				Optional: true,
 				Elem: &schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"id": &schema.Schema{
@@ -283,7 +289,7 @@ func resourceAccessRule() *schema.Resource {
 						},
 						"name": &schema.Schema{
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"version": &schema.Schema{
 							Type:     schema.TypeString,
@@ -301,12 +307,197 @@ func resourceAccessRule() *schema.Resource {
 }
 
 func resourceAccessRuleCreate(d *schema.ResourceData, m interface{}) error {
-//	cf := m.(*goftd.FTD)
-//	n := *goftd.AccessRule{}
-//	log.Println("==============> ", n)
-//	a := new(goftd.AccessRule)
+	cf := m.(*goftd.FTD)
+	//n := *goftd.AccessRule{}
+	//log.Println("==============> ", n)
+	// Define all the Structures and associated variables
+	pAR := new(goftd.AccessRule)
+
+	// Name
+	pAR.Name = d.Get("name").(string)
+
+	// Source Zones
+	psourcezones := d.Get("sourcezones").(*schema.Set)
+	var localSourceZoneObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range psourcezones.List() {
+		val := vRaw.(map[string]interface{})
+
+		localSourceZoneObject = append(localSourceZoneObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.SourceZones = localSourceZoneObject
+
+	// Destination Zones
+	pdestinationzones := d.Get("destinationzones").(*schema.Set)
+	var localDestinationZoneObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range pdestinationzones.List() {
+		val := vRaw.(map[string]interface{})
+
+		localDestinationZoneObject = append(localDestinationZoneObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.DestinationZones = localDestinationZoneObject
+
+	// Source Networks
+	psourcenetworks := d.Get("sourcenetworks").(*schema.Set)
+	var localSourceNetworkObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range psourcenetworks.List() {
+		val := vRaw.(map[string]interface{})
+
+		localSourceNetworkObject = append(localSourceNetworkObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.SourceNetworks = localSourceNetworkObject
+
+	// Destination Networks
+	pdestinationnetworks := d.Get("destinationnetworks").(*schema.Set)
+	var localDestinationNetworkObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range pdestinationnetworks.List() {
+		val := vRaw.(map[string]interface{})
+
+		localDestinationNetworkObject = append(localDestinationNetworkObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.DestinationNetworks = localDestinationNetworkObject
+
+	// Source Ports
+	psourceports := d.Get("sourceports").(*schema.Set)
+	var localSourcePortObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range psourceports.List() {
+		val := vRaw.(map[string]interface{})
+
+		localSourcePortObject = append(localSourcePortObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.SourcePorts = localSourcePortObject
+
+	// Destination Ports
+	pdestinationports := d.Get("destinationports").(*schema.Set)
+	var localDestinationPortObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range pdestinationports.List() {
+		val := vRaw.(map[string]interface{})
+
+		localDestinationPortObject = append(localDestinationPortObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.DestinationPorts = localDestinationPortObject
+
+	// Rule Action
+	pAR.RuleAction = d.Get("ruleaction").(string)
+
+	// Event Log Action
+	pAR.EventLogAction = d.Get("eventlogaction").(string)
+
+	// Vlan Tags
+	pvlantags := d.Get("vlantags").(*schema.Set)
+	var localVlanTagObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range pvlantags.List() {
+		val := vRaw.(map[string]interface{})
+
+		localVlanTagObject = append(localVlanTagObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.VLANTags = localVlanTagObject
+
+	// Users
+	pusers := d.Get("users").(*schema.Set)
+	var localUsersObject = []*goftd.ReferenceObject{}
+	for _, vRaw := range pusers.List() {
+		val := vRaw.(map[string]interface{})
+
+		localUsersObject = append(localUsersObject, &goftd.ReferenceObject{
+			ID:      val["id"].(string),
+			Version: val["version"].(string),
+			Name:    val["name"].(string),
+			Type:    val["type"].(string),
+		})
+	}
+	pAR.Users = localUsersObject
 
 
+//// WORK needs to be done
+/*
+	// Intrusion Policy
+	pintrusionpolicy := d.Get("intrusionpolicy").(*schema.Set)
+	var localIntrusionPolicyObject = new(goftd.ReferenceObject)
+	for _, vRaw := range pintrusionpolicy.List() {
+		val := vRaw.(map[string]interface{})
+
+		localIntrusionPolicyObject.ID = val["id"].(string)
+		localIntrusionPolicyObject.Version = val["version"].(string)
+		localIntrusionPolicyObject.Name = val["name"].(string)
+		localIntrusionPolicyObject.Type = val["type"].(string)
+	}
+	pAR.IntrusionPolicy = localIntrusionPolicyObject
+
+	// File Policy
+	pfilepolicy := d.Get("filepolicy").(*schema.Set)
+	var localFilePolicyObject = new(goftd.ReferenceObject)
+	for _, vRaw := range pfilepolicy.List() {
+		val := vRaw.(map[string]interface{})
+
+		localFilePolicyObject.ID = val["id"].(string)
+		localFilePolicyObject.Version = val["version"].(string)
+		localFilePolicyObject.Name = val["name"].(string)
+		localFilePolicyObject.Type = val["type"].(string)
+	}
+	pAR.FilePolicy = localFilePolicyObject
+
+	// Log Files
+	pAR.LogFiles = d.Get("logfiles").(bool)
+
+	// Syslog Server
+	psyslogserver := d.Get("syslogserver").(*schema.Set)
+	var localSyslogServerObject = new(goftd.ReferenceObject)
+	for _, vRaw := range psyslogserver.List() {
+		val := vRaw.(map[string]interface{})
+
+		localSyslogServerObject.ID = val["id"].(string)
+		localSyslogServerObject.Version = val["version"].(string)
+		localSyslogServerObject.Name = val["name"].(string)
+		localSyslogServerObject.Type = val["type"].(string)
+	}
+	pAR.SyslogServer = localSyslogServerObject
+//// Works need to be done
+	// Parent
+*/
+
+	// Call CreateAccessRule
+	err := cf.CreateAccessRule(pAR, "default" )
+	if err != nil{
+		log.Println("==== > Error %s",err)
+		return err
+	}
+
+	d.SetId(pAR.ID)
 	return resourceServerRead(d, m)
 }
 
