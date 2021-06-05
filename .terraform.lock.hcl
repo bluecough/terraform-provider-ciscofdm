8a4bd7da66d6e291b18ba60f847a62e4ba296d95
bluecough <9311254+bluecough@users.noreply.github.com>
2021-06-04 20:50:14 -0400

    Added

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -4,6 +4,6 @@
 provider "registry.terraform.io/hashicorp/ciscofdm" {
   version = "1.0.0"
   hashes = [
-    "h1:rvbDVsXNu8ma1gx4fEkK4/CyPDp24a46R3K5BPjhRQc=",
+    "h1:jcNRzD0QQ3fSJUXHO5s9BZ5XrjMhEmKTivQiXKkZqmM=",
   ]
 }
--- resource_deploy.go
+++ resource_deploy.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	goftd "github.com/bluecough/go-ftd"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"log"
 )
 
 func resourceDeployObject() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceDeployCreate,
-//		Delete: resourceDeployDelete,
+		Read:   resourceDeployRead,
+		Update: resourceDeployUpdate,
+		Delete: resourceDeployDelete,
 
 		Schema: map[string]*schema.Schema{
 			"name": &schema.Schema{
@@ -29,6 +32,32 @@ func resourceDeployObject() *schema.Resource {
 }
 
 func resourceDeployCreate(d *schema.ResourceData, m interface{}) error {
+	cf := m.(*goftd.FTD)
+
+	n := new(goftd.DeployObject)
+	n.Name = d.Get("name").(string)
+	n.SubType = d.Get("subtype").(string)
+	n.Value = d.Get("value").(string)
+
+	err := cf.PostDeploy(n, 1)
+	if err != nil {
+		fmt.Errorf("error: %s\n", err)
+	}
+	return resourceDeployCreate(d, m)
+}
+func resourceDeployRead(d *schema.ResourceData, m interface{}) error {
+	//cf := m.(*goftd.FTD)
+	log.Println("GS DEBUG === NetworkObjectRead== id ", d.Id())
+	//cf.GetNetworkObjectByID(d.Id())
+	return nil
+}
+func resourceDeployUpdate(d *schema.ResourceData, m interface{}) error {
+	//cf := m.(*goftd.FTD)
+	log.Println("GS DEBUG === NetworkObjectRead== id ", d.Id())
+	//cf.GetNetworkObjectByID(d.Id())
+	return nil
+}
+func resourceDeployDelete(d *schema.ResourceData, m interface{}) error {
 	cf := m.(*goftd.FTD)
 
 	n := new(goftd.DeployObject)
