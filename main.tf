9a9251e11d78272f96946c17f4537400b6254427
bluecough <9311254+bluecough@users.noreply.github.com>
2021-06-08 20:11:54 -0400

    Minor update on main.tf file

--- main.tf
+++ main.tf
@@ -1,32 +1,33 @@
 provider "ciscofdm" {
-  api_url = "192.168.128.1"
+  api_url = "192.168.128.188"
   username = "admin"
   password = "C1sco12345!"
   ssl_no_verify = true 
 }
-
-#resource "ciscofdm_dummy" "my-server" {
-#  address = "1.2.3.4"
-#}
 
 #resource "ciscofdm_networkobject" "myobject" {
 #  name = "GS-Terraform"
 #  subtype = "HOST"
 #  value = "2.2.2.3"
 #}
+resource "ciscofdm_networkobject" "GSNetwork" {
+  name = "GSNetwork"
+  subtype = "NETWORK"
+  value = "10.10.10.0/24"
+}
 
-#resource "ciscofdm_networkobjectgroup" "mygroup" {
-#  name = "NetworkGroupGS"
-#  objects {
-#     netname = "any-ipv6"
-#     type = "networkobject"
-#  }
-#  objects {
-#     netname = "any-ipv4"
-#     type = "networkobject"
-#  }
-#  type = "networkobjectgroup"
-#}
+resource "ciscofdm_networkobjectgroup" "mygroup" {
+  name = "NetworkGroupGS"
+  objects {
+     netname = "any-ipv6"
+     type = "networkobject"
+  }
+  objects {
+     netname = "any-ipv4"
+     type = "networkobject"
+  }
+  type = "networkobjectgroup"
+}
 #resource "ciscofdm_portobject" "GSPORT_4444" {
 #  name = "GSPORT 4444-5000"
 #  layer4 = "TCP"
@@ -45,19 +46,19 @@ provider "ciscofdm" {
 #    type = "tcpportobject"
 #  }
 #}
-resource "ciscofdm_accessrule" "myaccessrules01" {
-  name = "GSTERRAFORMRULE001"
-  ruleaction = "PERMIT"  
-  intrusionpolicy = {
-    name = "Connectivity Over Security"
-    type = "intrusionpolicy"
-  }
-}
+#resource "ciscofdm_accessrule" "myaccessrules01" {
+#  name = "GSTERRAFORMRULE001"
+#  ruleaction = "PERMIT"  
+#  intrusionpolicy = {
+#    name = "Connectivity Over Security"
+#    type = "intrusionpolicy"
+#  }
+#}
 resource "ciscofdm_accessrule" "myaccessrules02" {
   name = "GSTERRAFORMRULE002"
   ruleaction = "PERMIT"
   sourcenetworks {
-   name = "192.168.126.0"
+   name = "GSNetwork"
    type = "networkobject"
   } 
   destinationnetworks {
